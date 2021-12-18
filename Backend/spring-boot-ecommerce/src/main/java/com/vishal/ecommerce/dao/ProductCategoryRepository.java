bc4b42bbacb716ae3f447f91227eb28c8601baeb
Vishal Kumar <vishal.kumar3@publicissapient.com>
2021-12-18 14:37:49 +0530

    create rest api with Spring Data Rest and JPA

---
+++ Backend/spring-boot-ecommerce/src/main/java/com/vishal/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,15 @@
+package com.vishal.ecommerce.dao;
+
+import com.vishal.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+/*name of json = productCategory and path="/product-category"
+Generally JPA makes plural of the class name that we pass in <JpaRepository<ProductCategory,Long>
+but here it won't make as ProductCategories rather it will do as ProductCategorys so kind of confusing
+so used @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+when we hit localhost:8080/api will be able to understand more
+*/
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
+}
