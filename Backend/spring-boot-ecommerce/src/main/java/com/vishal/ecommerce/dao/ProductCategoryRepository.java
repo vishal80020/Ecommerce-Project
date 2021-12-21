3a13d67695a04d3f05ec9cd85cf2fe1a3f7c442f
Vishal Kumar <vishal.kumar3@publicissapient.com>
2021-12-21 21:17:09 +0530

    add support for cross origin

--- Backend/spring-boot-ecommerce/src/main/java/com/vishal/ecommerce/dao/ProductCategoryRepository.java
+++ Backend/spring-boot-ecommerce/src/main/java/com/vishal/ecommerce/dao/ProductCategoryRepository.java
@@ -3,6 +3,7 @@ package com.vishal.ecommerce.dao;
 import com.vishal.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 /*name of json = productCategory and path="/product-category"
@@ -11,5 +12,10 @@ but here it won't make as ProductCategories rather it will do as ProductCategory
 so used @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 when we hit localhost:8080/api will be able to understand more
 */
+
+
+//origin = protocol + hostname + port
+
+@CrossOrigin("http://localhost:4200") //Accept calls from web browser scripts from this origin
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 }
--- Backend/spring-boot-ecommerce/src/main/java/com/vishal/ecommerce/dao/ProductRepository.java
+++ Backend/spring-boot-ecommerce/src/main/java/com/vishal/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,10 @@ package com.vishal.ecommerce.dao;
 
 import com.vishal.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//origin = protocol + hostname + port
+
+@CrossOrigin("http://localhost:4200") //Accept calls from web browser scripts from this origin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
