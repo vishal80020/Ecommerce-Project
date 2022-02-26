c8a57b98c5cfca258ec0312b155df20a642f874d
Vishal Kumar <vishal.kumar3@publicissapient.com>
2022-02-26 22:56:42 +0530

    add strict checking for attributes

--- FrontEnd/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ FrontEnd/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -4,31 +4,31 @@ import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+	selector: 'app-product-details',
+	templateUrl: './product-details.component.html',
+	styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
 
-  product: Product = new Product(); //new Product() to avoid undefined error/Race Condition
+	product: Product = new Product(); //new Product() to avoid undefined error/Race Condition
 
-  constructor(private productService: ProductService,
-              private route: ActivatedRoute) { }
+	constructor(private productService: ProductService,
+		private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-    this.route.paramMap.subscribe(()=>{
-      this.handleProductDetails();
-    })
-  }
-  handleProductDetails() {
-    //get the id param string and convert it to number using + symbol
-    const theProductId: number = + this.route.snapshot.paramMap.get('id');
-    this.productService.getProduct(theProductId).subscribe(
-      data => {
-        this.product = data;
-      }
-    )
+	ngOnInit(): void {
+		this.route.paramMap.subscribe(() => {
+			this.handleProductDetails();
+		})
+	}
+	handleProductDetails() {
+		//get the id param string and convert it to number using + symbol
+		const theProductId: number = + this.route.snapshot.paramMap.get('id');
+		this.productService.getProduct(theProductId).subscribe(
+			data => {
+				this.product = data;
+			}
+		)
 
-  }
+	}
 
 }
