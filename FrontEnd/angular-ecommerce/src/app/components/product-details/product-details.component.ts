ce2a5637f5e503cd5562412221ce58ede78b5f3e
Vishal Kumar <vishal.kumar3@publicissapient.com>
2022-05-09 20:09:20 +0530

    feature add to cart

--- FrontEnd/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ FrontEnd/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,7 +15,8 @@ export class ProductDetailsComponent implements OnInit {
 	product: Product = new Product(); //new Product() to avoid undefined error/Race Condition
 
 	constructor(private productService: ProductService,
-		private route: ActivatedRoute) { }
+				private cartSerivce: CartService,			
+				private route: ActivatedRoute) { }
 
 	ngOnInit(): void {
 		this.route.paramMap.subscribe(() => {
@@ -29,6 +32,12 @@ export class ProductDetailsComponent implements OnInit {
 			}
 		)
 
+	}
+
+	addToCart() {
+		console.log(`Adding to Cart: ${this.product.name}, ${this.product.unitPrice}`);
+		const theCartItem = new CartItem(this.product);
+		this.cartSerivce.addToCart(theCartItem);
 	}
 
 }
---
+++ FrontEnd/angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class CartService {
+
+	cartItems: CartItem[] = [];
+
+	totalPrice: Subject<number> = new Subject<number>();
+	totalQuantity: Subject<number> = new Subject<number>();
+
+
+	constructor() { }
+
+	addToCart(theCartItem: CartItem) {
+
+		// check if we already have item in our cart
+		let alreadyExistsInCart: boolean = false;
+		let existingCartItem: CartItem = undefined;
+
+		// check if cart is empty or not
+		if (this.cartItems.length > 0) {
+
+			// if cart is not empty
+			//find whether item currently being added is present in cart already
+			// search through all items in cart
+			for(let tempCartItem of this.cartItems){
+				if(tempCartItem.id === theCartItem.id) {
+					//found the item currently being added in the cart is
+					//already present 
+					existingCartItem = tempCartItem; // here tempCartItem reference is passed to existingCartItem
+					alreadyExistsInCart = true;
+					break;
+				}
+			}
+		}
+		if(alreadyExistsInCart) {
+			// we have found that item is already present in cart so no need to add the item itself
+			//just increase the quantity of it
+			existingCartItem.quantity++;
+		} else {
+			// add item to the cart
+			this.cartItems.push(theCartItem);
+		}
+
+		// then compute total price and total quantity
+		this.computeCartTotals();
+	}
+	computeCartTotals() {
+		let totalPriceValue: number = 0;
+		let totalQuantityValue: number = 0;
+
+		for(let currentCartItem of this.cartItems) {
+			totalPriceValue += currentCartItem.unitPrice*currentCartItem.quantity;
+			totalQuantityValue += currentCartItem.quantity;
+		}
+
+		// publish the new values... all subscribers will receive the new data that's why used subject
+		this.totalPrice.next(totalPriceValue); //.next will publish or send the event
+		this.totalQuantity.next(totalQuantityValue);
+
+		//log cart data just for debugging purpose
+		this.logCartData(totalPriceValue,totalQuantityValue);
+	}
+	logCartData(totalPriceValue: number, totalQuantityValue: number) {
+		console.log(`Contents of the cart: `);
+		for(let tempCartItem of this.cartItems){
+			const subTotalPrice = tempCartItem.quantity*tempCartItem.unitPrice;
+			console.log(`name: ${tempCartItem.name},quantity= ${tempCartItem.quantity},unitPrice= ${tempCartItem.unitPrice},subTotalPrice = ${subTotalPrice}`);
+		}
+		console.log(`totalPriceValue: ${totalPriceValue.toFixed(2)},quantity: ${totalQuantityValue}`);
+		console.log(`******************************`);
+	}
+}
