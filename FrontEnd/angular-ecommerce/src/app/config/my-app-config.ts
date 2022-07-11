2c6bdca30e3000a76f58dd891bd83e378e0088f3
Vishal Kumar <vishal.kumar3@publicissapient.com>
2022-07-11 20:00:01 +0530

    add login and logout

---
+++ FrontEnd/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa5qb74gzTZskSUl5d7',
+        issuer: 'https://dev-44192877.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
